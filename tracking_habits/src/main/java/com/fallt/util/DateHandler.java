676e67628d2e176958c889611a2f636e81cf4bbf
Andrei Guzei <aaguzey@gmail.com>
2024-10-12 15:16:21 +1000

    рефакторинг DateHandler

--- tracking_habits/src/main/java/com/fallt/util/DateHandler.java
+++ tracking_habits/src/main/java/com/fallt/util/DateHandler.java
@@ -11,21 +11,11 @@ public class DateHandler {
     private DateHandler() {
     }
 
-    /**
-     * Метод конвертирует строку в дату в соответствии с используемым паттерном
-     * @param date Дата в виде строки
-     * @return Дата в виде объекта класса LocalDate
-     */
     public static LocalDate getDateFromString(String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
         return LocalDate.parse(date, formatter);
     }
 
-    /**
-     * Метод осуществляет проверку введенной строки на соответствие формату даты
-     * @param input Строка
-     * @return Результат проверки строки
-     */
     public static boolean checkInputDate(String input) {
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern(DATE_PATTERN);
         try {
